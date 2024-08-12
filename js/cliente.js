4b304d1bacde30c251780d567867122d756a0870
Lucas da Silva <lucas2005sf@gmail.com>
2024-08-12 20:58:33 -0300

    alteracao 12/08

---
+++ js/cliente.js
@@ -0,0 +1,3 @@
+document.getElementById("cliente").addEventListener("click", function() {
+ window.location.href = "/cliente/client.html"; // Substitua pela URL desejada
+});
---
+++ js/vendedor.js
@@ -0,0 +1,3 @@
+document.getElementById("vendedor").addEventListener("click", function() {
+ window.location.href = "/vendedor/vendedor.html"; // Substitua pela URL desejada
+});
